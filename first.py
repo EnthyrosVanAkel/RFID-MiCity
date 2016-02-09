011d25cc640a8b6a053ab3e280002dfc0d18b748
Omar Vladimir Galicia Palmeros <thewakosmanclub@gmail.com>
2016-02-09 16:29:34 -0600

    first

--- first.py
+++ first.py
@@ -1,21 +1,12 @@
-#!/usr/bin/env python
-import time
-import sys
+import serial
+serial = serial.Serial("/dev/ttyAMA0", baudrate=9600)
 
-card = '0019171125'        # Stored good card number consider using a list or a file.
+code = ''
 
-def main():                # define a main function.
-    while True:            # loop until the program encounters an error.
-        sys.stdin = open('/dev/tty0', 'r')
-        RFID_input = input()            
-        if RFID_input == card:      # compare the stored number to the input and if True execute code.
-            print "Access Granted" 
-            print "Read code from RFID reader:{0}".format(RFID_input)
-        else:                    # and if the condition is false excecute this code.
-            print "Access Denied"
-
-# where is tty defined??
-            tty.close()
-
-if __name__ == '__main__':
-    main()
+while True:
+    data = serial.read()
+    if data == '\r':
+        print(code)
+        code = ''
+    else:
+        code = code + data
