8f104c4e9024fa3e3770033ce9bf9cbe49d86622
Omar Vladimir Galicia Palmeros <thewakosmanclub@gmail.com>
2016-02-10 15:48:00 -0600

    neuvo

--- osc-server.py
+++ osc-server.py
@@ -7,5 +7,5 @@ def handler(addr, tags, data, client_address):
 
 if __name__ == "__main__":
     s = OSC.OSCServer(('127.0.0.1', 57120))  # listen on localhost, port 57120
-    s.addMsgHandler('/startup', handler)     # call handler() for OSC messages received with the /startup address
+    s.addMsgHandler('/checkout', handler)     # call handler() for OSC messages received with the /startup address
     s.serve_forever()
