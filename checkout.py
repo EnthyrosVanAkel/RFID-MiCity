3c7ae7a93aeabaa569d8804ad10b8a2950ed1f7d
Omar Vladimir Galicia Palmeros <thewakosmanclub@gmail.com>
2016-02-11 14:09:44 -0600

    Tdo bine

--- checkout.py
+++ checkout.py
@@ -12,7 +12,7 @@ def send_message(message):
 	cliente = OSC.OSCClient()
 	cliente.connect((URL,PORT))
 	oscmsg = OSC.OSCMessage()
-	oscmsg.setAddress("/checkout")
+	oscmsg.setAddress("/micity_checkout")
 	oscmsg.append(message)
 	cliente.send(oscmsg)
 
@@ -20,7 +20,7 @@ def send_message(message):
 
 if __name__ == '__main__':
     buffer = ''
-    ser = serial.Serial('/dev/ttyUSB0', BITRATE, timeout=0)
+    ser = serial.Serial('/dev/ttyUSB0', BITRATE, timeout=1)
 rfidPattern = re.compile(b'[\W_]+')
 visitante = 12
 eleccion = 10
@@ -31,8 +31,10 @@ while True:
         lines = buffer.split('\n')
         last_received = lines[-2]
         match = rfidPattern.sub('', last_received)
-	mensaje = '[pulsera:'+match+',id_visitante:'+str(visitante)+',eleccion_id:'+str(eleccion)+']'
-        print mensaje
+	hexa = match[4:10]
+	decimal = int(hexa,16)
+	mensaje = 'pulsera:'+str(decimal)+',id_visitante:'+str(visitante)+',eleccion_id:'+str(eleccion)
+        print decimal
         send_message(mensaje)
         buffer = ''
         lines = ''
--- osc-server.py
+++ osc-server.py
@@ -7,5 +7,8 @@ def handler(addr, tags, data, client_address):
 
 if __name__ == "__main__":
     s = OSC.OSCServer(('127.0.0.1', 5720))  # listen on localhost, port 5720
-    s.addMsgHandler('/checkout', handler)     # call handler() for OSC messages received with the /startup address
-    s.serve_forever()
+    s.addMsgHandler('/micity_checkout', handler)     # call handler() for OSC messages received with the /checkout address
+s.addMsgHandler('/micity_checkin', handler)     # call handler() for OSC messages received with the /checkin address
+s.addMsgHandler('/bici_checkout',handler)
+s.addMsgHandler('/bici_checkin', handler)  
+s.serve_forever()
