9cc72189341966a67309cb53eba4437ebe2c6c8b
Omar Vladimir Galicia Palmeros <thewakosmanclub@gmail.com>
2016-06-15 17:28:14 -0500

    as

--- rojo.py
+++ rojo.py
@@ -15,32 +15,36 @@ URL = 'http://papalote.cocoplan.mx/v0/visitante'
 SCR_SIZE = 320, 240
 WHITE = (255, 255, 255)
 
+pygame.init()
+pygame.mouse.set_visible(False)
+screen = pygame.display.set_mode(SCR_SIZE)
     
 
 if __name__ == '__main__':
     buffer = ''
     ser = serial.Serial('/dev/ttyUSB0', BITRATE, timeout=1)
     rfidPattern = re.compile(b'[\W_]+')
+    visitante = 12
+    eleccion = 10
     while True:
-      # Read data from RFID reader
-      buffer = buffer + ser.read(ser.inWaiting())
-      if '\n' in buffer:
-        lines = buffer.split('\n')
-        last_received = lines[-2]
-        match = rfidPattern.sub('', last_received)
-        hexa = match[4:10]
-        de cimal = int(hexa,16)
-        print decimal
-        data = {'rfid':decimal,'zona':1,'experiencia':1}
-        r = requests.get(URL,params = data)
-        json = r.json()
-        edad = json.get('edad')
-        if (edad < '18'):
-            print 'No permitido'
-        else:
-            print 'Permitido'
+        buffer = buffer + ser.read(ser.inWaiting())
+        if '\n' in buffer:
+            lines = buffer.split('\n')
+            last_received = lines[-2]
+            match = rfidPattern.sub('', last_received)
+            hexa = match[4:10]
+            decimal = int(hexa,16)
+            print decimal
+            data = {'rfid':decimal,'zona':1,'experiencia':1}
+            r = requests.get(URL,params = data)
+            json = r.json()
+            edad = json.get('edad')
+            if (edad < '18'):
+                print 'No permitido'
+            else:
+                print 'Permitido'
             
-        buffer = ''
-        lines = ''
+            buffer = ''
+            lines = ''
 
-    pygame.display.update()
+        pygame.display.update()
