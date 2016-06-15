abc3d1f8c49581d52a169e08e69a34deec000f5a
Omar Vladimir Galicia Palmeros <thewakosmanclub@gmail.com>
2016-06-15 13:42:27 -0500

    as

--- as.py
+++ as.py
@@ -118,33 +118,6 @@ def main(argv):
 if __name__ == "__main__":
     main(sys.argv)
 
-    buffer = ''
-    ser = serial.Serial('/dev/ttyUSB0', BITRATE, timeout=1)
-    rfidPattern = re.compile(b'[\W_]+')
-    while True:
-      # Read data from RFID reader
-      buffer = buffer + ser.read(ser.inWaiting())
-      if '\n' in buffer:
-        lines = buffer.split('\n')
-        last_received = lines[-2]
-        match = rfidPattern.sub('', last_received)
-        hexa = match[4:10]
-        decimal = int(hexa,16)
-        print decimal
-        data = {'rfid':decimal,'zona':1,'experiencia':1}
-        r = requests.get(URL,params = data)
-        json = r.json()
-        edad = json.get('edad')
-        if (edad < '18'):
-            draw_button(buttons[1], True)
-            print 'No permitido'
-        else:
-            draw_button(buttons[0], True)
-            print 'Permitido'
-            
-        buffer = ''
-        lines = ''
-
 
 while 1:
     for event in pygame.event.get():
