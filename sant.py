68f0c58613ad3b856b0a1704f4f4870e7986e087
Omar Vladimir Galicia Palmeros <thewakosmanclub@gmail.com>
2016-06-16 12:26:14 -0500

    as

---
+++ sant.py
@@ -0,0 +1,34 @@
+import re, sys, signal, os, time, datetime
+import serial
+### Instalar Requets
+import requests
+import json
+import pygame
+
+BITRATE = 9600
+URL = 'http://papalote.cocoplan.mx/v0/visitante'
+SCR_SIZE = 320, 240
+WHITE = (255, 255, 255)
+
+
+
+if __name__ == '__main__':
+    buffer = ''
+    ser = serial.Serial('/dev/ttyUSB0', BITRATE)
+    rfidPattern = re.compile(b'[\W_]+')
+
+    try:
+        ser.flushInput()
+        ser.flushOutput()
+    except Exception, e:
+        print "error open serial port: " + str(e)
+        exit()
+​
+    if ser.isOpen():
+        while True:
+            line = ser.readline()
+            rfid = line.strip()
+            print rfid
+    
+
+
