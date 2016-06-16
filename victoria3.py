593d08556cdaea1ff057b2a03c373abe3ff53f03
Omar Vladimir Galicia Palmeros <thewakosmanclub@gmail.com>
2016-06-16 15:36:20 -0500

    New R

--- victoria3.py
+++ victoria3.py
@@ -5,9 +5,9 @@ import requests
 import pygame
 
 
-os.environ["SDL_FBDEV"] = "/dev/fb1"
-os.environ["SDL_MOUSEDEV"] = "/dev/input/touchscreen"
-os.environ["SDL_MOUSEDRV"] = "TSLIB"
+#os.environ["SDL_FBDEV"] = "/dev/fb1"
+#os.environ["SDL_MOUSEDEV"] = "/dev/input/touchscreen"
+#os.environ["SDL_MOUSEDRV"] = "TSLIB"
 
 BITRATE = 9600
 URL = 'http://papalote.cocoplan.mx/v0/visitante'
@@ -37,23 +37,22 @@ if __name__ == '__main__':
         print "error open serial port: " + str(e)
         exit()
 
-    if ser.isOpen():
-        while True:
-            line = ser.readline()
-            rfid = line.strip()
-            print rfid
-            data = {'rfid':rfid,'zona':1,'experiencia':1}
-            r = requests.get(URL,params = data)
-            json = r.json()
-            edad = json.get('edad')
-            if (edad < '18'):
-              pygame.draw.rect(screen,GREEN,RECT_GREE)
-              print 'No permitido'
-            else:
-              pygame.draw.rect(screen,GREEN,RECT_GREEN)
-              print 'Permitido'
+    while True:
+        line = ser.readline()
+        rfid = line.strip()
+        print rfid
+        data = {'rfid':rfid,'zona':1,'experiencia':1}
+        r = requests.get(URL,params = data)
+        json = r.json()
+        edad = json.get('edad')
+        if (edad < '18'):
+          pygame.draw.rect(screen,GREEN,RECT_GREE)
+          print 'No permitido'
+        else:
+          pygame.draw.rect(screen,GREEN,RECT_GREEN)
+          print 'Permitido'
 
 
-            screen.fill(BLACK)
+        screen.fill(BLACK)
 
-            pygame.display.update()
+        pygame.display.update()
