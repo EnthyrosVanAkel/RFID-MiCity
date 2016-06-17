edb193b89efa1006c111597f2e891b60d31cbe4a
Omar Vladimir Galicia Palmeros <thewakosmanclub@gmail.com>
2016-06-16 22:12:29 -0500

    Nrgo

--- Santi/victoria_chela_v.py
+++ Santi/victoria_chela_v.py
@@ -56,17 +56,17 @@ if __name__ == '__main__':
           pygame.draw.rect(screen,GREEN,RECT)
           c = requests.post(URL + 'free/' + match + '/true')
           time.sleep(2)
-          pygame.draw.rect(screen,BLACK,RECT)
+          #pygame.draw.rect(screen,BLACK,RECT)
         else:
           print 'Entregada'
           pygame.draw.rect(screen,BLUE,RECT)
           time.sleep(2)
-          pygame.draw.rect(screen,BLACK,RECT)
+          #pygame.draw.rect(screen,BLACK,RECT)
       else:
         print 'No Permitido'
         pygame.draw.rect(screen,RED,RECT)
         time.sleep(2)
-        pygame.draw.rect(screen,BLACK,RECT)
+        #pygame.draw.rect(screen,BLACK,RECT)
 
 
       pygame.display.update()
