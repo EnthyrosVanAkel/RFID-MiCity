5dfc511bce3531e9dff31c9d93962bbc954f012b
spoony <santiago.russek@gmail.com>
2016-06-17 02:14:52 +0000

    as

--- Santi/victoria_chela_v.py
+++ Santi/victoria_chela_v.py
@@ -3,12 +3,31 @@ import serial
 ### Instalar Requets
 import requests
 import json
+import pygame
+
+os.environ["SDL_FBDEV"] = "/dev/fb1"
+os.environ["SDL_MOUSEDEV"] = "/dev/input/touchscreen"
+os.environ["SDL_MOUSEDRV"] = "TSLIB"
 
 
 BITRATE = 9600
 
+SCR_SIZE = 320, 240
+WHITE = (255, 255, 255)
+BLACK = (0,0,0)
+RED = (255,0,0)
+GREEN = (0,255,0)
+YELLOW = (0,0,255)
+RECT_RED = [20, 0, 250, 80]
+RECT_GREEN = [20, 80, 250, 80]
+RECT_YELLOW = [20, 160, 250, 80]
 
-URL = 'http://10.1.8.170:9000/api/visitors/'
+
+pygame.init()
+pygame.mouse.set_visible(False)
+screen = pygame.display.set_mode(SCR_SIZE)
+
+URL = 'http://10.1.8.170:9000/api/visitors/edad/'
 
 if __name__ == '__main__':
   ser = serial.Serial('/dev/ttyUSB0', BITRATE)
@@ -26,7 +45,7 @@ if __name__ == '__main__':
       last_received = line.strip()
       match = rfidPattern.sub('', last_received)
       print match
-      cadena = URL + 'edad/' +match
+      cadena = URL + match
       r = requests.put(cadena)
       json = r.json()
       print json
@@ -34,12 +53,19 @@ if __name__ == '__main__':
       chela = json.get('chelaFree')
 
       if edad:
-        if not chela:
+        if !chela:
           print 'Permitido'
-	  c = requests.post(URL + 'free/' + match + '/true')
+          pygame.draw.rect(screen,GREEN,RECT_GREEN)
         else:
           print 'Entregada'
+          pygame.draw.rect(screen,YELLOW,RECT_YELLOW)
       else:
         print 'No Permitido'
+        pygame.draw.rect(screen,RED,RECT_RED)
+
+
+      pygame.display.update()
+
+
 
 
